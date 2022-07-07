437c09658a2c8f4365537f8a35d44c8c6177419b
Your Name <you@example.com>
2022-07-07 12:56:16 +0300

    Classwork

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,22 +7,26 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Navigation from './Comonent/Navigations';
-import Homes from './Comonent/Homes';
-import Abouts from './Comonent/Abouts';
-import Services from './Comonent/Services';
-import Blog from './Comonent/Blog';
+// import About from './Comonent/About';
+import Services from './Compp.js/Services';
+// import Home from './Comonent/Home';
+import Contacts from './Compp.js/Contacts';
+import Login from './Compp.js/Login';
+import Signup from './Compp.js/Signup';
+import Nav from './Compp.js/Nav';
 
 function App() {
 
   const [content, setContent] = useState()
   const [users, setUsers] = useState([])
+  const [openModal, setOpenModal] = useState(false)
 
   useEffect(() => {
     getContent()
   }, [])
 
   useEffect(() => {
+
     getUsers()
   }, [])
 
@@ -51,14 +55,19 @@ function App() {
   return (
     <div className='container'>
       <BrowserRouter>
-     <Navigation/>
+      <Nav setOpenModal={setOpenModal}/>
+   
+      <Login />
         <Routes>
-        <Route path='/' element={<Homes/>} />
-        <Route path='/Abouts' element={<Abouts/>} />
-        <Route path='/Services' element={<Services/>} />
-        <Route path='/Blog' element={<Blog/>} />
+          {/* <Route path='/' element={<Home />}/> */}
+          {/* <Route path='/about' element={<About />} /> */}
+          <Route path='/services' element={<Services />} />
+          <Route path='/contacts' element={<Contacts />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
         </Routes>
       </BrowserRouter>
+      <h1>Data</h1>
       <p>{content}</p>
 
       <table>
@@ -69,6 +78,80 @@ function App() {
 }
 
 export default App
+
+
+
+// import React, { useEffect, useState } from 'react'
+// import axios from "axios";
+
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+// import Navigation from './Comonent/Navigations';
+// import Homes from './Comonent/Homes';
+// import Abouts from './Comonent/Abouts';
+// import Services from './Comonent/Services';
+// import Blog from './Comonent/Blog';
+
+// function App() {
+
+//   const [content, setContent] = useState()
+//   const [users, setUsers] = useState([])
+
+//   useEffect(() => {
+//     getContent()
+//   }, [])
+
+//   useEffect(() => {
+//     getUsers()
+//   }, [])
+
+//   const getContent = async () => {
+//     const response = await axios.get('http://localhost:3000/');
+//     setContent(response.data);
+//   }
+
+//   const getUsers  = async () => {
+//     const response = await axios.get('http://localhost:3000/users')
+//     setUsers(response.data)
+//   }
+
+//   const listUsers = users.map(item => {
+//     return(
+//       <tbody key={item.id}>
+//         <tr>
+//           <td>{item.id}</td>
+//           <td>{item.name}</td>
+//           <td>{item.age}</td>
+//         </tr>
+//       </tbody>
+//     )
+//   })
+
+//   return (
+//     <div className='container'>
+//       <BrowserRouter>
+//      <Navigation/>
+//         <Routes>
+//         <Route path='/' element={<Homes/>} />
+//         <Route path='/Abouts' element={<Abouts/>} />
+//         <Route path='/Services' element={<Services/>} />
+//         <Route path='/Blog' element={<Blog/>} />
+//         </Routes>
+//       </BrowserRouter>
+//       {/* <p>{content}</p> */}
+
+//       <table>
+//           {listUsers}
+//       </table>
+//     </div>
+//   )
+// }
+
+// export default App
 
 // function App() {
 //   return (
--- frontend/src/Compp.js/Nav.js
+++ frontend/src/Compp.js/Nav.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-function Navigation() {
+function Nav({setOpenModal}) {
   return (
     <div className='container'>
-        <nav className="navbar navbar-expand-lg bg-light">
+        {/* <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/home">MYAPP</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,43 +24,19 @@ function Navigation() {
                     <li className="nav-item">
                     <Link className="nav-link" to='/Blog'>Blog</Link>
                     </li>
-                    <li className="nav-item">
-                  
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-                    </li>
-                </ul>
+                    <li className="nav-item"></li>
+                    </ul>
                 <form className="d-flex" role="search">
                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-success" type="submit">Search</button>
                 </form>
+                <div><button type='button' onClick={() => {setOpenModal(true)} } className="btn btn-outline-primary mx-2">Login</button></div>
+                <div><Link to='/signup'><button type='button' className="btn btn-outline-primary">Signup</button></Link></div>
                 </div>
             </div>
-</nav>
+        </nav> */}
     </div>
   )
 }
 
-export default Navigation
+export default Nav
